a4be2680d748911cd9d37b2a972512f229180083
arvinazwan <43392669+arvinazwan@users.noreply.github.com>
2018-10-08 19:58:15 +0700

    Add files via upload

---
+++ PBO3-10117105-Latihan35-ObjectOrientedProgramTunjangan/src/Karyawan.java
@@ -0,0 +1,27 @@
+//NAMA  :Arvi Nazwan Alhafiz 
+//NIM   :10117105    
+//KELAS :IF-3
+
+/**
+ *
+ * @author RIA FITRIANI
+ */
+public class Karyawan {
+    public String status;
+    public double gajiPokok;
+    public double tunjangan;
+
+    public double HitungTunjangan(String parStatus, double parGajiPokok){
+    return tunjangan=(parStatus.equals("Menikah")?0.35*parGajiPokok:0); }
+
+    public double HitungTotalGaji(double parGajiPokok, double parTunjangan){
+    return tunjangan = parGajiPokok+parTunjangan;}
+
+    public void HasilHitung(String parStatus, double parGajipokok){
+        System.out.println("\n=========Hasil Perhitungan Gaji Anda=========");
+        System.out.println("Gaji Pokok \t: Rp "+gajiPokok);
+        System.out.println("Tunjangan \t: Rp "+HitungTunjangan(parStatus,parGajipokok));
+        System.out.println("Total Gaji \t: Rp "+HitungTotalGaji(parGajipokok,tunjangan));
+        System.out.println("(Developed by : Rizki Adam Kurniawan)");
+    }
+}
---
+++ PBO3-10117105-Latihan35-ObjectOrientedProgramTunjangan/src/main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+//NAMA  :Arvi Nazwan Alhafiz 
+//NIM   :10117105    
+//KELAS :IF-3
+
+/**
+ *
+ * @author RIA FITRIANI
+ */
+public class main {
+        public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        Karyawan kar = new Karyawan();
+
+        System.out.println("===========Program Tunjangan===========");
+        System.out.print("Berapa gaji pokok Anda perbulan ? : Rp. ");
+        kar.gajiPokok = scn.nextDouble();
+        System.out.print("Status Anda? (Menikah/Belum) :");
+        kar.status = scn.next();
+
+        kar.HasilHitung(kar.status,kar.gajiPokok);
+
+    }
+
+    
+}
